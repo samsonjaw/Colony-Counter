740b841dce691a6db34eb8a6672f03d22c7d7aa7
samson <114964564+samsonjaw@users.noreply.github.com>
2024-05-03 01:24:16 +0800

    Update colony_count_main.py

--- colony_count_main.py
+++ colony_count_main.py
@@ -35,29 +35,36 @@ def select_circle_roi(image_with_grid, original_image):
         
     return masked_image
 
-
-
 def convert_to_gray_and_blackhat(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (23, 23))
     blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, kernel)
     return blackhat
 
 def apply_threshold(image):
-    _, binary = cv2.threshold(image, 40, 255, cv2.THRESH_BINARY)
+    image = cv2.GaussianBlur(image, (5, 5), 0)
+    _, binary = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return binary
 
 def remove_noise(image):
     kernel = np.ones((3,3), np.uint8)
     opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel, iterations=2)
     return opening
+ 
 
 def prepare_for_watershed(image):
     sure_bg = cv2.dilate(image, np.ones((3,3), np.uint8), iterations=3)
-    dist_transform = cv2.distanceTransform(image, cv2.DIST_L2, 5)
-    _, sure_fg = cv2.threshold(dist_transform, 0.05*dist_transform.max(), 255, 0)
+
+
+    dist_transform = cv2.distanceTransform(image, cv2.DIST_L2, 5) 
+
+    _, sure_fg = cv2.threshold(dist_transform, 0.3 * dist_transform.max(), 255, 0)
+    
     sure_fg = np.uint8(sure_fg)
     unknown = cv2.subtract(sure_bg, sure_fg)
+
+    cv2.imshow('test', sure_fg)
+
     return sure_fg, unknown
 
 def apply_watershed(image, sure_fg, unknown):
@@ -74,13 +81,13 @@ def apply_watershed(image, sure_fg, unknown):
     
     for i in range(1, num_labels):
         cX, cY = int(centroids[i][0]), int(centroids[i][1])
-        cv2.circle(centers_image, (cX, cY), 1, (0, 255, 0), -1)
+        cv2.circle(centers_image, (cX, cY), 2, (255, 255, 0), -1)
         #cv2.putText(centers_image, str(i), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
     n_colonies = num_labels - 1
     return boundaries_image, centers_image, n_colonies
 
-def resize_image(image, max_side=1000):
+def resize_image(image, max_side=1500):
     h, w = image.shape[:2]
 
     scale = max_side / max(h, w)
@@ -93,7 +100,7 @@ def resize_image(image, max_side=1000):
 def main():
     image_path = select_image()
     image = cv2.imread(image_path)
-    image = resize_image(image)
+    image = resize_image(image,1500)
     cv2.imshow('Resized Image', image)
     cv2.waitKey(0)
 
@@ -103,18 +110,29 @@ def main():
     cv2.waitKey(0)
     
     blackhat_image = convert_to_gray_and_blackhat(cropped_image)
-    cv2.imshow('Blackhat Image', blackhat_image)
+    cv2.imshow('blackhat_image', blackhat_image)
     cv2.waitKey(0)
     
     binary_image = apply_threshold(blackhat_image)
-    cv2.imshow('Binary Image', binary_image)
+    cv2.imshow('binary_image', binary_image)
     cv2.waitKey(0)
+
+    #binary_image = remove_noise(binary_image)
+    #cv2.imshow('Denoised Image', binary_image)
+    #cv2.waitKey(0)
 
     sure_fg, unknown = prepare_for_watershed(binary_image)
     
+
+    cropped_image = resize_image(cropped_image,5500)
+    sure_fg = resize_image(sure_fg,5500)
     boundaries_image, centers_image, n_colonies = apply_watershed(cropped_image, sure_fg, unknown)
     print(f"num: {n_colonies}")
-    cv2.imshow('Boundaries', boundaries_image)
+
+    boundaries_image = resize_image(boundaries_image,1500)
+    centers_image = resize_image(centers_image,1500)
+
+    #cv2.imshow('Boundaries', boundaries_image)
     cv2.imshow('Centers', centers_image)
     cv2.waitKey(0)
     
